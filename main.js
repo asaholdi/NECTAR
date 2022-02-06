2bbbe1f3ecfa9e7f52fa8c6fed3b5ed6ac03a029
Саша Штоколов <81267831+asaholdi@users.noreply.github.com>
2022-02-07 01:53:22 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,60 @@
-const prev = document.getElementsByClassName('.btn-prev');
-const next = document.getElementsByClassName('.bnt-next');
-const slides = document.querySelectorAll('.slide');
+/*let offset = 0;
+const slides = document.querySelector('.slider-wrapper');
+
+let prev = document.querySelector('.btn-prev');
+if (prev) {
+    prev.addEventListener('click', function () {
+        offset = offset - 100;
+        if (offset <= -100) {
+            offset = 0;
+        }
+        slides.style.left = -offset + '%'
+    });
+}
+
+let next = document.querySelector('.bnt-next');
+if (next) {
+    next.addEventListener('click', function plustimes() {
+        offset = offset + 100;
+        if (offset > 100) {
+            offset = 0;
+        }
+        slides.style.left = -offset + '%'
+        
+    });
+}
+ 
+let offset = 0;
+const slides = document.querySelector('.slider-wrapper');
+let next = document.querySelector('.bnt-next');
+function timeslide(){
+    offset = offset + 100;
+        if (offset > 100) {
+            offset = 0;
+        }
+        slides.style.left = -offset + '%'
+}
+
+next.onclick = timeslide;
+setInterval(timeslide, 5500);
+
+let prev = document.querySelector('.btn-prev');
+function slideout(){
+    offset = offset + 100;
+        if (offset > 100) {
+            offset = 0;
+        }
+        slides.style.left = -offset + '%'
+}
+prev.onclick = slideout;
+*/
+let offset = 0;
+const slides = document.querySelector('.slider-wrapper');
+function timeslide(){
+    offset = offset + 100;
+        if (offset > 100) {
+            offset = 0;
+        }
+        slides.style.left = -offset + '%'
+}
+setInterval(timeslide, 5500);
