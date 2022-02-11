07984d197f7e28c46a4fd3d1d477c1666d4ed380
Саша Штоколов <81267831+asaholdi@users.noreply.github.com>
2022-02-12 01:16:01 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function slideout(){
         slides.style.left = -offset + '%'
 }
 prev.onclick = slideout;
-*/
+
 let offset = 0;
 const slides = document.querySelector('.slider-wrapper');
 function timeslide(){
@@ -57,4 +57,81 @@ function timeslide(){
         }
         slides.style.left = -offset + '%'
 }
-setInterval(timeslide, 5500);
+setInterval(timeslide, 5500); 
+document.querySelectorAll('.traks').onmouseenter = () => {
+    document.querySelectorAll('.traks').style.transform = 'scale(1.1)';
+}
+
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animONscroll);
+    function animONscroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add('_active');
+            } else {
+                animItem.classList.remove('_active');
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    animONscroll();
+
+} */
+
+$(function () {
+
+    $('.slider-wrapper').slick({
+        arrows: false,
+        speed: 700,
+        autoplay: true,
+        autoplaySpeed: 4500,
+        pauseOnHover: true,
+    })
+
+})
+
+new WOW().init();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollItems = document.querySelectorAll('.scroll-item')
+    const scrollAnimation = () => {
+        let windowCenter = (window.innerHeight / 2) + window.scrollY;
+        scrollItems.forEach(el => {
+            let scrollOffset = el.offsetTop + el.offsetHeight / 2;
+            if (windowCenter >= scrollOffset) {
+                el.classList.add('animation-class');
+                
+            } else {
+                el.classList.remove('animation-class');
+                
+            }
+        });
+
+    };
+    scrollAnimation();
+    window.addEventListener('scroll', () => {
+        scrollAnimation();
+    });
+});
+    
+    
